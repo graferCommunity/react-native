a6fafe84eba66564d6fd0894fa287746217dbc80
Indeepa Amiru <grafercommunity@gmail.com>
2024-07-26 00:38:37 +0530

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
 import { ImageBackground, View } from 'react-native';
-import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'native-base';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,60 +6,24 @@ import { HomeScreen } from './index';
 import { Account } from './account';
 import { AccountChanges } from './accountChanges';
 
-// Prevent the splash screen from auto-hiding
-SplashScreen.preventAutoHideAsync();
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const [navigationReady, setNavigationReady] = useState(false);
 
-  useEffect(() => {
-    alert('Done');
-    async function prepare() {
-      try {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-      setTimeout(() => {
-        setNavigationReady(true);
-      }, 3000);
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
 
   return (
     <View
       style={{ flex: 1 }}
-      onLayout={onLayoutRootView}
     >
       <ImageBackground style={{ flex: 1 }} source={require('../assets/images/splash.gif')}>
-        {navigationReady && (<>
-          <NavigationContainer independent={true}>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="HOME" component={HomeScreen} />
-              <Stack.Screen name="ACCOUNT" component={Account} />
-              <Stack.Screen name="ACCOUNT_CHANGES" component={AccountChanges} />
-            </Stack.Navigator>
-            <StatusBar hidden />
-          </NavigationContainer>
-        </>)}
+        <NavigationContainer independent={true}>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="HOME" component={HomeScreen} />
+            <Stack.Screen name="ACCOUNT" component={Account} />
+            <Stack.Screen name="ACCOUNT_CHANGES" component={AccountChanges} />
+          </Stack.Navigator>
+          <StatusBar hidden />
+        </NavigationContainer>
       </ImageBackground>
 
     </View >
